ee7b54406fce7ea7acba3daa5a69da14230288c8
Alaa Ebrahim <alaaebrahim387@gmail.com>
2024-08-08 05:31:12 +0300

    hot-fix

--- src/api/getWeeklySchedule.js
+++ src/api/getWeeklySchedule.js
@@ -1,12 +1,18 @@
 import { useState, useCallback } from "react";
 import axios from "axios";
+import { useAuth } from "../auth/authProvider";
 
 const url = process.env.REACT_APP_API_URL;
 
-const getSchedule = async (doctorId) => {
+const getSchedule = async (doctorId, token) => {
     try {
         const response = await axios.get(
-            `${url}/api/doctors/${doctorId}/schedule`
+            `${url}/api/doctors/${doctorId}/schedule`,
+            {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+            }
         );
         return response.data;
     } catch (error) {
@@ -18,11 +24,12 @@ export const useSchedule = () => {
     const [isLoading, setIsLoading] = useState(false);
     const [data, setData] = useState(null);
     const [error, setError] = useState(null);
+    const { token } = useAuth();
 
     const execute = useCallback(async (doctorId) => {
         setIsLoading(true);
         try {
-            const data = await getSchedule(doctorId);
+            const data = await getSchedule(doctorId, token);
             setData(data);
             setIsLoading(false);
             return data.sort((a) => a.weekDay);
